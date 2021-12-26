c8476dea300579b3ca7ab5674231913e2a4ae608
Igor <igorlyakhoff@gmail.com>
2021-12-27 00:58:43 +0300

    10point. Squash in one commit some commits each other add Subtraction, Division, Multiplication and SQRT method.

--- src/Adder.java
+++ src/Adder.java
@@ -2,4 +2,21 @@ public class Adder {
     public int add(int a, int b){
         return a+b;
     }
+    public int sub(int a, int b){
+        return a-b;
+    }
+    public int div(int a, int b){
+       try {
+           return a/b;
+       }catch (ArithmeticException arithmeticException){
+           System.out.println("division by zero");
+           return 0;
+       }
+    }
+    public int mult(int a, int b){
+        return a*b;
+    }
+    public double sqrt(double a){
+        return Math.sqrt(a);
+    }
 }
